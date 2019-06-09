ae786e9a69841198c773fb70f4874d2d4728d30c
Sara Farazi <s.farazi90@gmail.com>
2019-06-09 17:39:22 -0400

    fix query search caching functionlity

--- axe/data_provider.py
+++ axe/data_provider.py
@@ -1,22 +1,33 @@
 import redis
+import sqlite3
 
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
+DB_FILE_NAME = "db.sqlite3"
 KEYWORDS_TABLE_NAME = "KEYWORDS"
 
 
+def create_connection():
+    conn = sqlite3.connect(DB_FILE_NAME)
+    return conn
+
+
 def add_term_entry(cursor, term):
-    command = "SELECT * FROM {} WHERE TOKENS = {}".format(term)
+    command = "SELECT TOKENS FROM {} WHERE IMAGE = '{}'".format(KEYWORDS_TABLE_NAME, term)
     results = cursor.execute(command).fetchall()
     for image in results:
-        redis_client.sadd(term, image)
+        redis_client.sadd(term, image[0])
 
 
 def get_keyword_results(keywords):
+    conn = create_connection()
+    cursor = conn.cursor()
     for keyword in keywords:
         # if the data for this keyword has not been cached yet
         # load it into redis
         if redis_client.smembers(keyword) == set():
-            add_term_entry(keyword)
+            add_term_entry(cursor, keyword)
 
-        redis_client.sinter(keywords)
+    results = redis_client.sinter(keywords)
+    return list(results)
+
--- axe/views.py
+++ axe/views.py
@@ -1,30 +1,77 @@
+import re
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from axe.data_provider import get_keyword_results
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stop_words = set(stopwords.words("english"))
+
+PAGE_SIZE = 16
 
 
-# Create your views here.
+def get_keywords(query):
+    tokens = []
+    for token in word_tokenize(query):
+        if token not in stop_words and token.isalnum():
+            tokens.append(token)
+
+    return tokens
+
+
+def is_search_request(request):
+    return not request.GET == {}
+
+
+def validate_advanced_search(query, max_size, min_size):
+    if query == "" and min_size == "" and max_size == "":
+        return False
+
+    if min_size != "" and not min_size.isdecimal():
+        return False
+
+    if max_size != "" and not max_size.isdecimal():
+        return False
+
+    return True
+
+
+def get_results_page(results, page):
+    page_index = page - 1
+    return results[page_index*PAGE_SIZE:page_index*PAGE_SIZE+PAGE_SIZE]
+
+
 def index(request):
-    if request.method == 'POST':
-        if 'text_search' in request.POST:
-            text = request.POST['text']
-            if text == "":
-                return render(request, 'index.html', {
-                    'text_search_error': True
-                })
-            return render(request, 'results.html')
-        elif 'advanced_search' in request.POST:
-            keywords = request.POST['keywords']
-            min_size = request.POST['min_size']
-            max_size = request.POST['max_size']
-            if keywords == "" and min_size == "" and max_size == "":
-                return render(request, 'index.html', {
-                    'advanced_search_error': True
-                })             
-            return render(request, 'results.html')
-        else:
+    if request.method == 'GET':
+
+        if not is_search_request(request):
             return render(request, 'index.html')
 
+        query = request.GET['query']
+        min_size = request.GET['min_size']
+        max_size = request.GET['max_size']
+
+        if not validate_advanced_search(query, max_size, min_size):
+            return render(request, 'index.html', {
+                'advanced_search_error': True
+            })
+
+        keywords = get_keywords(query)
+        results = [result.decode("utf-8") for result in get_keyword_results(keywords)]
+
+        try:
+            page = int(request.GET['page'])
+        except Exception:
+            page = 1
+
+        return render(request, 'results.html', {
+            "results": get_results_page(results, page)
+        })
+
     else:
-        return render(request, 'index.html')
+        return HttpResponse("Invalid request")
 
 
 def search_results(request):
