34f00fe03585e6b4300d7196b93d7afce85657e8
Sara Farazi <s.farazi90@gmail.com>
2019-06-09 17:38:16 -0400

    add static files urls

--- shopify/urls.py
+++ shopify/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from axe.views import index, search_results
+from shopify import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('results', search_results),
     path('', index)
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
